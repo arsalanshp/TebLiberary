0ae7e054ff309f5e101905fd4dc1d6193b7aed14
v.karimi <v.karimi@gmail.com>
2016-08-08 16:03:12 +0330

    first page

--- app/src/main/java/library/tebyan/com/teblibrary/model/MetadataList.java
+++ app/src/main/java/library/tebyan/com/teblibrary/model/MetadataList.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Created by v.karimi on 7/26/2016.
  */
-public class MetaDataList {
+public class MetadataList {
     public ArrayList<Metadata> Result;
 
     public ArrayList<Metadata> getResult() {
