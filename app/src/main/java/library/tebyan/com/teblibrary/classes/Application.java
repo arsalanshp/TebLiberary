f89687099daf69cde0230566e2a09bcc4d44aeec
v.karimi <v.karimi@gmail.com>
2016-08-08 15:27:39 +0330

    1.5 l

---
+++ app/src/main/java/library/tebyan/com/teblibrary/classes/Application.java
@@ -0,0 +1,110 @@
+package library.tebyan.com.teblibrary.classes;
+
+import android.util.Log;
+
+import com.koushikdutta.ion.Ion;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import library.tebyan.com.teblibrary.R;
+
+/**
+ * Created by F.piri on 1/17/2016.
+ */
+
+
+
+public class Application extends android.app.Application {
+    public static Ion ion;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /*TypefaceUtil.overrideFont(getApplicationContext(), "SERIF",
+                "fonts/IRAN Sans Light.ttf");*/
+        CertificateFactory cf = null;
+        try {
+            cf = CertificateFactory.getInstance("X.509");
+            InputStream caInput = getResources().openRawResource(R.raw.cer_tebyan);
+            Certificate ca = cf.generateCertificate(caInput);
+            caInput.close();
+            KeyStore keyStore = KeyStore.getInstance("BKS");
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+            TrustManager[] wrappedTrustManagers = getWrappedTrustManagers(tmf.getTrustManagers());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, wrappedTrustManagers, null);
+            ion= Ion.getDefault(this);
+            ion.configure().createSSLContext("TLS");
+            ion.getHttpClient().getSSLSocketMiddleware().setSSLContext(sslContext);
+            ion.getHttpClient().getSSLSocketMiddleware().setTrustManagers(wrappedTrustManagers);
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // cert file stored in \app\src\main\res\raw
+
+    }
+
+
+
+    private TrustManager[] getWrappedTrustManagers(TrustManager[] trustManagers) {
+        final X509TrustManager originalTrustManager = (X509TrustManager) trustManagers[0];
+        return new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return originalTrustManager.getAcceptedIssuers();
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            if (certs != null && certs.length > 0){
+                                certs[0].checkValidity();
+                            } else {
+                                originalTrustManager.checkClientTrusted(certs, authType);
+                            }
+                        } catch (CertificateException e) {
+                            Log.w("checkClientTrusted", e.toString());
+                        }
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            if (certs != null && certs.length > 0){
+                                certs[0].checkValidity();
+                            } else {
+                                originalTrustManager.checkServerTrusted(certs, authType);
+                            }
+                        } catch (CertificateException e) {
+                            Log.w("checkServerTrusted", e.toString());
+                        }
+                    }
+                }
+        };
+    }
+
+}
