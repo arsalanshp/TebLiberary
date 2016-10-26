f7ae9383c58b88abc51b92ca70d0e3e5069d6ffb
yahyapour <samaneh_yahyapour@yahoo.com>
2016-10-26 15:13:05 +0330

    complete of searchFragment UI and filters ( spinners)

--- app/src/main/java/library/tebyan/com/teblibrary/fragment/menus/SearchFragment.java
+++ app/src/main/java/library/tebyan/com/teblibrary/fragment/menus/SearchFragment.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -24,12 +27,15 @@ import library.tebyan.com.teblibrary.R;
 
 public class SearchFragment extends Fragment implements View.OnClickListener ,OnItemSelectedListener {
 
-    Spinner spinnerFilter1;
-    Spinner spinnerFilter2;
-    Spinner spinnerFilter3;
-    Spinner spinnerFilter4;
-    Spinner logical_spinner1;
-    Spinner logical_spinner2;
+    Spinner spinner_filter2;
+    Spinner spinner_filter3;
+    Spinner spinner_filter1;
+    Spinner logic_spinner1;
+    Spinner logic_spinner2;
+    Spinner sort_base_spinner;
+    Spinner sort_type_spinner;
+    Button drop_down_filtes_btn;
+    LinearLayout linearLayout_filters;
 
     View view ;
     List<String> filtersList;
@@ -55,74 +61,73 @@ public class SearchFragment extends Fragment implements View.OnClickListener ,On
         // Inflate the layout for this fragment
         view =inflater.inflate(R.layout.fragment_search, container, false);
         context = view.getContext();
-        initData();
+        initUI();
         return view;
     }
 
+    private void initUI(){
+        initSpinners();
+
+        drop_down_filtes_btn = (Button)view.findViewById(R.id.drop_down_filtes_btn);
+        linearLayout_filters = (LinearLayout)view.findViewById(R.id.linearLayout_filters);
+        drop_down_filtes_btn.setOnClickListener(this);
+
+    }
 
     private void initData(){
 
-        initSpinners();
+
 
     }
 
     private void initSpinners(){
+        spinner_filter2 = (Spinner) view.findViewById(R.id.spinner_filter2);
+        spinner_filter3 = (Spinner) view.findViewById(R.id.spinner_filter3);
+        spinner_filter1 = (Spinner) view.findViewById(R.id.spinner_filter1);
+        logic_spinner1 = (Spinner) view.findViewById(R.id.logic_spinner1);
+        logic_spinner2 = (Spinner) view.findViewById(R.id.logic_spinner2);
+        sort_base_spinner = (Spinner) view.findViewById(R.id.sort_base_spinner);
+        sort_type_spinner = (Spinner) view.findViewById(R.id.sort_type_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,
+                R.array.search_filters, android.R.layout.simple_spinner_item);
 
-        spinnerFilter1 = (Spinner) view.findViewById(R.id.spinner_filter1);
-        spinnerFilter1.setOnItemSelectedListener(this);
-
-        spinnerFilter2 = (Spinner) view.findViewById(R.id.spinner_filter2);
-        spinnerFilter2.setOnItemSelectedListener(this);
-
-        spinnerFilter3 = (Spinner) view.findViewById(R.id.spinner_filter3);
-        spinnerFilter3.setOnItemSelectedListener(this);
-//
-//        spinnerFilter4 = (Spinner) view.findViewById(R.id.spinner_filter4);
-//        spinnerFilter4.setOnItemSelectedListener(this);
-
-//        logical_spinner1 = (Spinner) view.findViewById(R.id.logical_spinner1);
-//        logical_spinner1.setOnItemSelectedListener(this);
-
-        // Spinner Drop down elements
-        filtersList = new ArrayList<String>();
-        filtersList.add("عنوان");
-        filtersList.add("شرح پدیدآور");
-        filtersList.add("موضوع");
-        filtersList.add("ناشر");
-        filtersList.add("یادداشت");
-        filtersList.add("زبان");
-        filtersList.add("نوع منبع");
-        filtersList.add("تمام متن");
-        filtersList.add("تمام موارد");
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, filtersList);
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // attaching data adapter to spinner
-        spinnerFilter1.setAdapter(dataAdapter);
-        spinnerFilter2.setAdapter(dataAdapter);
-        spinnerFilter3.setAdapter(dataAdapter);
-
-
-        ///////logical spinner /////////
-
-        logical_spinner1 = (Spinner) view.findViewById(R.id.logical_spinner1);
-        logical_spinner1.setOnItemSelectedListener(this);
-
-        logicsList = new ArrayList<String>();
-        logicsList.add("و");
-        logicsList.add("یا");
-        logicsList.add("بجز");
-
-        ArrayAdapter<String> logicalDataAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, logicsList);
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        logical_spinner1.setAdapter(logicalDataAdapter);
+        ArrayAdapter<CharSequence> logic_adapter = ArrayAdapter.createFromResource(context,
+                R.array.logical_search_filters, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> sort_base_adapter = ArrayAdapter.createFromResource(context,
+                R.array.sort_base_search, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence>  sort_type_adapter = ArrayAdapter.createFromResource(context,
+                R.array.sort_type, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner_filter1.setAdapter(adapter);
+        spinner_filter2.setAdapter(adapter);
+        spinner_filter3.setAdapter(adapter);
+        logic_spinner1.setAdapter(logic_adapter);
+        logic_spinner2.setAdapter(logic_adapter);
+        sort_base_spinner.setAdapter(sort_base_adapter);
+        sort_type_spinner.setAdapter(sort_type_adapter);
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.drop_down_filtes_btn:
+                if (drop_down_filtes_btn.getTag()=="close"){
+                    linearLayout_filters.setVisibility(View.VISIBLE);
+                    drop_down_filtes_btn.setTag("open");
+                    drop_down_filtes_btn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_arrow_drop_up_black_24dp, 0, 0, 0);
+                }
+                else{
+                    linearLayout_filters.setVisibility(View.GONE);
+                    drop_down_filtes_btn.setTag("close");
+                    drop_down_filtes_btn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.arrow_drop_down_black_24dp, 0, 0, 0);
+
+                }
+                break;
+
+        }
 
     }
 
