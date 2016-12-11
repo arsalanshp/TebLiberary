4d37a2d976f7230873f6efd8adf7161387f5c415
yahyapour <samaneh_yahyapour@yahoo.com>
2016-12-11 18:44:33 +0330

    fix xml file of askQuestion fragment

--- app/src/main/java/library/tebyan/com/teblibrary/fragment/menus/AskQuestionFragment.java
+++ app/src/main/java/library/tebyan/com/teblibrary/fragment/menus/AskQuestionFragment.java
@@ -15,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
@@ -37,14 +38,15 @@ public class AskQuestionFragment extends Fragment implements View.OnClickListene
 
     private View view;
     private Context context;
-    private EditText userQuestionTxt,userName,userAge,userEmail, password;
+    private EditText userQuestionTxt,userName,userAge,userEmail;
+    private TextView password;
     private RadioGroup radioButtonGroup;
     private CheckBox userUnKnow;
     private Spinner userEducationSpinner;
-    private Button sendQuestionBTN,cancelQuestionBTN;
+    private Button sendQuestionBTN,cancelQuestionBTN ,closeQuestionBTN;
     private String name, age,email,question;
     private String[] userEducationTag;
-    private LinearLayout passwordLayout;
+    private LinearLayout passwordLayout,sendLineareLayout;
 
 
     @Override
@@ -65,8 +67,9 @@ public class AskQuestionFragment extends Fragment implements View.OnClickListene
 
 
     private void initUI() {
+        sendLineareLayout =(LinearLayout)view.findViewById(R.id.send_lineare_layout);
         passwordLayout =(LinearLayout)view.findViewById(R.id.password_layout);
-        password = (EditText)view.findViewById(R.id.password);
+        password = (TextView)view.findViewById(R.id.password);
         userQuestionTxt=(EditText)view.findViewById(R.id.user_question_txt);
         userName=(EditText)view.findViewById(R.id.user_name);
         userAge=(EditText)view.findViewById(R.id.user_age);
@@ -78,6 +81,8 @@ public class AskQuestionFragment extends Fragment implements View.OnClickListene
         userEducationSpinner=(Spinner)view.findViewById(R.id.user_education_spinner);
         cancelQuestionBTN=(Button)view.findViewById(R.id.cancel_question_btn);
         cancelQuestionBTN.setOnClickListener(this);
+        closeQuestionBTN = (Button)view.findViewById(R.id.close_question_btn);
+        closeQuestionBTN.setOnClickListener(this);
 
     }
 
@@ -94,13 +99,11 @@ public class AskQuestionFragment extends Fragment implements View.OnClickListene
 
         switch (v.getId()) {
             case R.id.send_question_btn:
-
-
-                    if (validation()){
-                        int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
-                        RadioButton radioButton =(RadioButton)radioButtonGroup.findViewById(radioButtonID);
-                        String url="";
-                        try{
+                if (validation()){
+                    int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
+                    RadioButton radioButton =(RadioButton)radioButtonGroup.findViewById(radioButtonID);
+                    String url="";
+                    try{
                          url = WebserviceUrl.CONSULTATION_CREATE_KNOWN+
                                 "?Name="+URLEncoder.encode(name,"utf-8")+
                                 "&Email="+email+
@@ -113,34 +116,48 @@ public class AskQuestionFragment extends Fragment implements View.OnClickListene
                                 "&Gender="+radioButton.getTag().toString()+
                                 "&ShowInSite="+ Integer.toString(!(userUnKnow.isChecked())?1:0)+
                                 "&OutputType=2";}
-catch (UnsupportedEncodingException e){
-                Log.e("",e.getMessage().toString());
-            }
-                        Globals.ion.with(context).load(url)
-//                                .asString()
-//                                .setCallback(new FutureCallback<String>(){
-//                                    @Override
-//                                    public void onCompleted(Exception e, String result) {
-//                                        Toast.makeText(context,result, Toast.LENGTH_LONG).show();
-//                                        }
-//                                    });
-                                .as(ConsultationResult.class)
-                                .setCallback(new FutureCallback<ConsultationResult>(){
-                                    @Override
-                                    public void onCompleted(Exception e, ConsultationResult result) {
-                                        if(result.getResult().getError()== null) {
-                                            userQuestionTxt.setText("");
-                                            password.setText(result.getResult().getObject().getPassword());
-                                            passwordLayout.setVisibility(View.VISIBLE);
-                                        }
-                                        else {
-                                            Toast.makeText(context,result.getResult().getError(),Toast.LENGTH_SHORT).show();
-                                        }
-                                    }});
-                    }
+                    catch (UnsupportedEncodingException e){
+                                    Log.e("",e.getMessage().toString());
+                                }
+                    Globals.ion.with(context).load(url)
+    //                                .asString()
+    //                                .setCallback(new FutureCallback<String>(){
+    //                                    @Override
+    //                                    public void onCompleted(Exception e, String result) {
+    //                                        Toast.makeText(context,result, Toast.LENGTH_LONG).show();
+    //                                        }
+    //                                    });
+                            .as(ConsultationResult.class)
+                            .setCallback(new FutureCallback<ConsultationResult>(){
+                                @Override
+                                public void onCompleted(Exception e, ConsultationResult result) {
+                                    if(result.getResult().getError()== null) {
+                                        userQuestionTxt.setText("");
+                                        String text = "کاربر گرامی پسورد شما"+
+                                                result.getResult().getObject().getPassword()+
+                                                " می باشد"+
+                                                "\n"+
+                                                "شما میتوانید از این پسورد جهت پیگیری سوالات خود در سایت استفاده نمایید"+
+                                                "\n"+
+                                                "با تشکر از شما"+
+                                                "موسسه فرهنگی اطلاع رسانی تبیان "
+                                                ;
+//                                        String text = "کاربر گرامی پسورد شما " +result.getResult().getObject().getPassword() + "می باشد. شما میتوانید از این پسورد جهت پیگیری پرسش های خود در آینده استفاده نمایید. با تشکر از شما ";
+                                        password.setText(text);
+                                        passwordLayout.setVisibility(View.VISIBLE);
+                                        sendLineareLayout.setVisibility(View.GONE);
+                                    }
+                                    else {
+                                        Toast.makeText(context,result.getResult().getError(),Toast.LENGTH_SHORT).show();
+                                    }
+                                }});
+                }
 
                 break;
             case R.id.cancel_question_btn:
+                getActivity().onBackPressed();
+                break;
+            case R.id.close_question_btn:
                 getActivity().onBackPressed();
                 break;
         }
@@ -152,15 +169,15 @@ catch (UnsupportedEncodingException e){
         age = userAge.getText().toString();
         email = userEmail.getText().toString();
         question = userQuestionTxt.getText().toString();
-        if(name == null){
+        if(name == null || name.isEmpty()){
             userName.setError("این فیلد ضروری است");
             continueFlag = false;
         }
-        if(age == null || age.length() > 3){
+        if(age == null || age.length() > 3 || age.isEmpty()){
             userAge.setError("لطفا سن خود را به درستی وارد کنید");
             continueFlag = false;
         }
-        if(email== null){
+        if(email== null || email.isEmpty()){
             userEmail.setError("لطفا ایمیل معتبر وارد نمایید");
             continueFlag = false;
         }
